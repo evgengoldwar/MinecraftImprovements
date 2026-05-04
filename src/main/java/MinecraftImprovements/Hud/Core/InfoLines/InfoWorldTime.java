f2679b51d117abccc2a1a1759f54c451772905bb
evgengoldwar <evgeniy10102003kuptsov@gmail.com>
2026-05-04 12:25:55 +0400

    Fix WorldTime

--- src/main/java/MinecraftImprovements/Hud/Core/InfoLines/InfoWorldTime.java
+++ src/main/java/MinecraftImprovements/Hud/Core/InfoLines/InfoWorldTime.java
@@ -17,10 +17,8 @@ public class InfoWorldTime extends InfoLine {
         long worldTime = world.getWorldTime();
 
         long totalSeconds = totalTicks / 20;
-        long remainingSeconds = totalSeconds % 86_400;
-        long totalHours = remainingSeconds / 3600;
-        long totalMinutes = (remainingSeconds % 3600) / 60;
-        long totalSecs = remainingSeconds % 60;
+        long totalMinutes = (totalSeconds % 3600) / 60;
+        long totalHours = totalSeconds / 3600;
 
         long adjustedTime = (worldTime + 6_000) % 24_000;
         long hours = adjustedTime / 1_000;
@@ -30,12 +28,14 @@ public class InfoWorldTime extends InfoLine {
         String nightSuffix = isNight ? " (" + EnumChatFormatting.DARK_GRAY + "Night" + EnumChatFormatting.RESET + ")"
             : "";
 
+        String formattedTotalHours = String.format("%,d", totalHours);
+
         if (totalHours > 0) {
-            return tr("info_worldtime.2", totalHours, String.format("%02d:%02d", hours, minutes), nightSuffix);
+            return tr("info_worldtime.2", formattedTotalHours, String.format("%02d:%02d", hours, minutes), nightSuffix);
         } else if (totalMinutes > 0) {
             return tr("info_worldtime.1", totalMinutes, String.format("%02d:%02d", hours, minutes), nightSuffix);
         } else {
-            return tr("info_worldtime.0", totalSecs, String.format("%02d:%02d", hours, minutes), nightSuffix);
+            return tr("info_worldtime.0", totalSeconds, String.format("%02d:%02d", hours, minutes), nightSuffix);
         }
     }
 
